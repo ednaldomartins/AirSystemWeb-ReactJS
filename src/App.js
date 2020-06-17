9ca8354e8231415c00cc2b7b6fbfeed5221afafd
Ednaldo <martinsednaldo@outlook.com>
2020-06-17 11:57:45 -0300

    Apresentação dos cards com as cidades.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,71 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
+import {Grid, Card, CardContent, Typography, makeStyles} from '@material-ui/core'
+import axios from 'axios'
 import './App.css';
 
 class App extends Component {
 
-  componentDidMount() {
-    fetch("http://localhost:3000/places", {method: "GET"})
-    .then(res => console.log(res))
+  constructor() {
+    super()
+    this.state = {
+      places: [],
+      flights: []
+    }
+  }
+  
+  async componentDidMount() {
+    await axios
+      .get("http://localhost:3000/places", {method: "GET"})
+      .then(res => {
+        this.setState( { places: res.data.places } )
+        console.log(res.data.places)
+      })
+
+    await axios
+      .get("http://localhost:3000/flight/all", {method: "GET"})
+      .then(res => {
+        this.setState( { flights: res.data.flights } )
+        console.log(res.data.flights)
+      })
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <grid> {
+        getComponentListPlace(this.state.places)
+      } </grid>
     );
   }
 
 }
+
+const getComponentListPlace = (props) => {
+  return (
+    <Grid container spacing={1}>
+    {
+      props.map(
+        place => (
+          <Grid item md={3}>
+            <Card >
+              <CardContent >
+                <Typography variant="h7">
+                  <p>País: {place.country}</p>
+                  <p>Estado: {place.estate}</p>
+                  <p>Cidade: {place.city}</p>
+                  <p>CEP: {place.zip_code}</p>  
+                </Typography>
+                <Typography variant="h8" color="textSecondary" align="right">
+                  identificador: {place.place_id}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        )
+      )
+    } 
+    </Grid>
+  )
+} 
 
 export default App;
